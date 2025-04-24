4616ea687610ebc79f52a5154eec6784553073b3
kieo23 <dankstar@gmail.com>
2025-04-24 11:37:13 +0900

    4.24 11시 2번쨰 ArrayList사용법

--- src/com/woori/BAM/Main3.java
+++ src/com/woori/BAM/Main3.java
@@ -1,4 +1,6 @@
 package com.woori.BAM;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Main3 {
 	public static void main(String[] args) {
@@ -7,8 +9,12 @@ public class Main3 {
 		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);  //스캐너 선언
        
+		//클래스 리모컨 = (생성자)new 객체;
+		
 		int id = 1; //번글 변수 생성
 		//이곳까지 1회실행후 while 반복문 시작
+		List<Article> articles = new ArrayList<>();//뒷 <>는 생략가능
+//		List<Article> articles = new ArrayList<Article>();
 		
 		
 		while (true) { //무한 반복문 시작
@@ -36,9 +42,31 @@ public class Main3 {
 				//공백없이 제목 입력방법
 				System.out.printf("내용 : ");
 				String body = sc.nextLine().trim(); 
+				
+//				Article article = new Article(); // 최적화 사용 안함 일때
+				Article article = new Article(id,title,body); //인자를 통해 생성자 호출 
+//				article.title = title;  
+//				article.body = body;    
+//				article.id = id;		
+				
+				articles.add(article); //List구조인 ArrayList 객체인 articles에 저장
+				//Article 타입으로 선언하여 article 리모컨이 들어간다
+				
+				// 최적화 2
+//				articles.add(new Article(id,title,body)); //두줄을 합쳐서 사용하는법
+				
 				System.out.println(id +"번글이 생성되었습니다");
 				id++;
 				//while문 밖의 지역변수의 id값을 불러와 1씩 더해줌
+				
+			} else if (cmd.equals("test")) {
+				for(int i = 0; i < articles.size(); i++) {
+					System.out.println(articles.get(i).id);
+					System.out.println(articles.get(i).title);
+					System.out.println(articles.get(i).body);
+					//get(i)로 ArrayList의 배열 번호속 정보를 끄집어낸다.
+					//get(1)일시 article 각가 3가지의 정보가 1이란 이름으로 등록된다.
+				}
 			}else {
 				System.out.println("존재하지 않는 명령어 입니다");
 				//모든 if문이 false처리가 된다면 else 문장 실행
@@ -50,4 +78,14 @@ public class Main3 {
 		//종료 메세지후 자바 출력 끝냄
 	}
 
+}
+class Article {
+	int id; //번호
+	String title; //제목
+	String body;  //내용
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
 }
