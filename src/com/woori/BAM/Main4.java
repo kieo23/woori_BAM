cc552ed6ef69068fbe9a4c2c180034bdb2faa136
kieo23 <dankstarflint@gmail.com>
2025-04-29 11:17:46 +0900

    과거 고민 흔적 코드 제거 테스트베이스v1 사용

--- src/com/woori/BAM/Main4.java
+++ src/com/woori/BAM/Main4.java
@@ -13,55 +13,15 @@ public class Main4 {
 		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);
 
-//		TestDate testdate = new TestDate();
-//		Article articlelist = new Article(lastArticleId, viewCheck, title, body,Util.getDateStr());
-		
 		List<Article> articles = new ArrayList<>();
-		
-//		int te = 0;
+
 		int lastArticleId = 1; // 게시글 번호, 마지막 게시글 번호 수정
-//		int testId = 0;
-//		int testViewCheck = 0;
-//		String testTitle = "제목";
-//		String testBody = null;
-		
-		
-//		Article ar1 = new Article(lastArticleId++,Util.getDateStr(),"제목1", "내용1", 10);
-//		articles.add(ar1);
-//		lastArticleId++;
-//		Article ar2 = new Article(lastArticleId++,Util.getDateStr(),"제목2", "내용2", 20);
-//		articles.add(ar2);
-//		lastArticleId++;
-//		Article ar3 = new Article(lastArticleId++,Util.getDateStr(),"제목3", "내용3", 30);
-//		articles.add(ar3);
-//		lastArticleId++;
-		
-		
-		//10번 반복
+		// 10번 반복
 		for (int i = 1; i <= 10; i++) {
-			articles.add(new Article(lastArticleId++,Util.getDateStr(),"제목"+ i, "내용"+ i, i * 10));
-			
+			articles.add(new Article(lastArticleId++, Util.getDateStr(), "제목" + i, "내용" + i, i * 10));
+
 		}
-		
-		
-//		articles.add(new Article(lastArticleId++,Util.getDateStr(),"제목1", "내용1", 10));
-//		articles.add(new Article(lastArticleId++,Util.getDateStr(),"제목2", "내용2", 20));
-//		articles.add(new Article(lastArticleId++,Util.getDateStr(),"제목3", "내용3", 30));
-		//최적화 2번 거침
-		
-//		List<TestDate> articlesTest = new ArrayList<>();
-//		
-//		for(int i = 1; i < 4; i++) {
-//			testViewCheck += 10;
-//			testId += 1;
-//			testTitle = "제목"+(te++);
-//			testBody = "내용"+(te++);
-//		testdate.Reset(testId, testViewCheck, testTitle, testBody,Util.getDateStr());
-//		articlesTest.add(testdate);
-//		}
-//		for (TestDate t : articlesTest) {
-//		System.out.printf("%d %s %s %d %s \n",t.testId,t.testTitle,t.testBody,t.testViewCek,t.testRegDate);
-//		}
+
 		while (true) {
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine().trim();
@@ -69,11 +29,10 @@ public class Main4 {
 			if (cmd.equals("exit")) {
 				break;
 			}
-			
+
 			int id = 0;
 			Article foundArticle = null;
-			
-			
+
 			if (cmd.length() == 0) {
 				System.out.println("명령어를 입력해 주세요");
 				continue;
@@ -85,19 +44,12 @@ public class Main4 {
 				String title = sc.nextLine().trim();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine().trim();
-				int viewCheck = 0; //새로 생성시 0으로 시작하도록 함
+				int viewCheck = 0; // 새로 생성시 0으로 시작하도록 함
 
-				
 				Util.getDateStr();
-//				LocalDateTime now = LocalDateTime.now(); //시간 설정
-//				String formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초"));
-//				String regDate = formatedNow;
-				
-				
-//				Article article = new Article(lastArticleId, title, body,regDate);
-				Article article = new Article(lastArticleId,Util.getDateStr(),title, body, viewCheck);
+
+				Article article = new Article(lastArticleId, Util.getDateStr(), title, body, viewCheck);
 				articles.add(article);
-				
 
 				System.out.println(lastArticleId + "번글이 생성되었습니다");
 				lastArticleId++;
@@ -109,23 +61,16 @@ public class Main4 {
 					continue;
 
 				}
-				
+
 				System.out.printf("번호    |     제목        |     내용      |                  날짜              |   조회수\n");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 //					System.out.printf("%d       |     %s       |     %s     |     %s, %s\n", article.id, article.title,article.body,calendar.get(article.id-1),timeMake);
-					System.out.printf("%d       |     %s       |     %s     |     %s|     %d\n", article.id, article.title,article.body,article.regDate,article.viewCheck);
+					System.out.printf("%d       |     %s       |     %s     |     %s|     %d\n", article.id,
+							article.title, article.body, article.regDate, article.viewCheck);
 				}
 			} else if (cmd.startsWith("article detail ")) { // article detail 로 시작하니?
 				String[] cmdBits = cmd.split(" "); // 문자 쪼개기칸
-													// (.)(,)(" ") 구분 칸을 적는다.
-//				System.out.println(cmdBits[0]);
-//				System.out.println(cmdBits[1]);
-//				System.out.println(cmdBits[2]);
-
-//				int checkIdNum = 0;
-
-//				int id;
 				try { // Exception ㅂ라생 할 예상 코드 블럭
 					id = Integer.parseInt(cmdBits[2]);
 //					checkIdNum = id;
@@ -138,8 +83,6 @@ public class Main4 {
 					e.printStackTrace();
 				}
 
-//				boolean articleChk = false;
-//				Article foundAticle = null;
 				for (Article article : articles) {
 
 					if (article.id == id) {
@@ -152,28 +95,27 @@ public class Main4 {
 					System.out.println(id + "번에 해당하는 게시물 없음");
 					continue; // 매우매우 중요. 아래에서 NullPointException 발생 안되게 조치
 				} // null값이 아니라면 아래 출력.
-				
-				foundArticle.viewCheck++; //조회수 1씩 증가
+
+				foundArticle.viewCheck++; // 조회수 1씩 증가
 				System.out.println("번호 : " + foundArticle.id);
 				System.out.println("날짜 : " + foundArticle.regDate);
 				System.out.println("제목 : " + foundArticle.title);
 				System.out.println("내용 : " + foundArticle.body);
 				System.out.println("조회수 : " + foundArticle.viewCheck);
-				
+
 			} else if (cmd.startsWith("article delete ")) {
 				String[] cmdBits = cmd.split(" ");
 //				int id = 0;
 //				Article foundAticle = null;
-				try { 
+				try {
 					id = Integer.parseInt(cmdBits[2]);
 
 				} catch (NumberFormatException e) {
 					System.out.println("삭제할 게시물의 숫자를 입력하세요.");
 					continue;
-				}catch (Exception e) { 
-						e.printStackTrace();
-					}
-//				id = Integer.parseInt(cmdBits[2]);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 				for (Article article : articles) {
 					if (article.id == id) {
 						articles.remove(id - 1);
@@ -182,66 +124,52 @@ public class Main4 {
 						foundArticle = article;
 						break;
 					}
-					}
-					if (foundArticle == null) {
-						System.out.println(id + "번 게시물이 존재하지 않습니다.");
-						continue;
-				
-					} 
+				}
+				if (foundArticle == null) {
+					System.out.println(id + "번 게시물이 존재하지 않습니다.");
+					continue;
 
-				
+				}
+
 			} else if (cmd.startsWith("article modify ")) {
 				String[] cmdBits = cmd.split(" ");
-//				int id = 0;
-//				Article foundAticle = null;
-				try { 
+				try {
 					id = Integer.parseInt(cmdBits[2]);
 
 				} catch (NumberFormatException e) {
 					System.out.println("수정 할 게시물의 숫자를 입력하세요.");
 					continue;
-				}catch (Exception e) { 
-						e.printStackTrace();
-					}
-				
-				for (Article article : articles) { //배열을 순서대로 돌림
-					if (article.id == id) { //순서가 일치할때 해당 인덱스의 정보 수정
-						foundArticle = article; //주소가 같기에 foundArticle, article 같은 주소가된다
-						
-//						System.out.printf("수정할 제목 : ");
-//						String correctionTitle = sc.nextLine().trim();
-//						foundArticle.title = correctionTitle; //두가지 모두 주소가 같기에 상관없다
-//						System.out.print("수정할 내용 : ");
-//						String correctionBody = sc.nextLine().trim();
-//						article.body = correctionBody;
-						
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				for (Article article : articles) { // 배열을 순서대로 돌림
+					if (article.id == id) { // 순서가 일치할때 해당 인덱스의 정보 수정
+						foundArticle = article; // 주소가 같기에 foundArticle, article 같은 주소가된다
+
 						System.out.printf("수정할 제목 : ");
-						foundArticle.title = sc.nextLine().trim();  //foundArticle와 article는 주소가 같도록 선언함
+						foundArticle.title = sc.nextLine().trim(); // foundArticle와 article는 주소가 같도록 선언함
 						System.out.printf("수정할 내용 : ");
-						article.body = sc.nextLine().trim();  // 따라서 불러오는 값은 같음
-						
-						
-						
+						article.body = sc.nextLine().trim(); // 따라서 불러오는 값은 같음
+
 						System.out.println("수정완료");
 						break;
-						
+
 					}
 				}
 				if (foundArticle == null) {
 					System.out.println(id + "번 게시물이 존재하지 않습니다.");
 					continue;
-			}
-			}
-			else {
+				}
+			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
 		}
-		
+
 		sc.close();
 		System.out.println("== 프로그램 종료 ==");
 	}
 }
-
 
 class Article {
 	int id;
@@ -250,7 +178,7 @@ class Article {
 	String regDate;
 	int viewCheck;
 
-	public Article(int id, String regDate,String title, String body,int viewCheck) {
+	public Article(int id, String regDate, String title, String body, int viewCheck) {
 		this.id = id;
 		this.title = title;
 		this.body = body;
@@ -258,33 +186,23 @@ class Article {
 		this.viewCheck = viewCheck;
 	}
 }
-class TestDate {
-	
-	int number = 0;
-	int testId;
-	String testRegDate;
-	String testTitle;
-	String testBody;
-	int testViewCek;
-//	public TestDate (int testId, int testViewCek, String testTitle, String testBody, String testRegDate) {
-//		this.testId = testId;
-//		this.testRegDate = testRegDate;
-//		this.testTitle = testTitle;
-//		this.testBody = testBody;
-//		this.testViewCek = testViewCek;
-		void Reset (int testId, int testViewCek, String testTitle, String testBody, String testRegDate){
-//			testViewCek += 10;
-//			testId += 1;
-//			testTitle = "제목"+(number++);
-//			testBody = "내용"+(number++);
-			this.testId = testId;
-			this.testRegDate = testRegDate;
-			this.testTitle = testTitle;
-			this.testBody = testBody;
-			this.testViewCek = testViewCek;
-
-			
-		}
-			
-		}
-
+//class TestDate {
+//	
+//	int number = 0;
+//	int testId;
+//	String testRegDate;
+//	String testTitle;
+//	String testBody;
+//	int testViewCek;
+//		void Reset (int testId, int testViewCek, String testTitle, String testBody, String testRegDate){
+//			this.testId = testId;
+//			this.testRegDate = testRegDate;
+//			this.testTitle = testTitle;
+//			this.testBody = testBody;
+//			this.testViewCek = testViewCek;
+//
+//			
+//		}
+//			
+//		}
+//
